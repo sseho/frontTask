a3a79bc84e5e9702f3fffd801f047325c0020092
Choi Seho <kindseho@naver.com>
2023-08-10 19:03:20 +0900

    delete: SearchAtom 삭제

--- front-task/recoil/Atom.js
+++ front-task/recoil/Atom.js
@@ -1,12 +1,5 @@
 import { atom } from "recoil";
 
-export const SearchAtom = atom({
-  key: "SearchAtom",
-  default: {
-    input: null,
-  },
-});
-
 export const LikeAtom = atom({
   key: "LikeAtom",
   default: [],
